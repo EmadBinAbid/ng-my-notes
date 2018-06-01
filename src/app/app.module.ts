fa345bb2908b4d90362443d39a8ec7cf19c061f8
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-06-01 18:12:21 +0500

    Updated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyNotesAppComponent } from './components/my-notes-app/my-notes-app.component';
+import { MyNotesListComponent } from './components/my-notes-list/my-notes-list.component';
+import { MyNotesEditorComponent } from './components/my-notes-editor/my-notes-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyNotesAppComponent
+    MyNotesAppComponent,
+    MyNotesListComponent,
+    MyNotesEditorComponent
   ],
   imports: [
     BrowserModule,
