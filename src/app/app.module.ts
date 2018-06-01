3825c13a99c239208e4c5c21e82c3dc6963ffb2a
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-06-01 18:09:17 +0500

    Added component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyNotesAppComponent } from './components/my-notes-app/my-notes-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNotesAppComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/my-notes-app/my-notes-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mn-my-notes-app',
+  templateUrl: './my-notes-app.component.html',
+  styleUrls: ['./my-notes-app.component.scss']
+})
+export class MyNotesAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
