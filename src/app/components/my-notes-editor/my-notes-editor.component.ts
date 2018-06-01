b200e15879c687083c2452a8a0ee540a02528bf3
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-06-01 18:13:55 +0500

    Added components.

---
+++ src/app/components/my-notes-editor/my-notes-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mn-my-notes-editor',
+  templateUrl: './my-notes-editor.component.html',
+  styleUrls: ['./my-notes-editor.component.scss']
+})
+export class MyNotesEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/my-notes-list/my-notes-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mn-my-notes-list',
+  templateUrl: './my-notes-list.component.html',
+  styleUrls: ['./my-notes-list.component.scss']
+})
+export class MyNotesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
